b3837f7f70be5fd3a663ed980e3c855847a4f7c7
lane-andreas <laaaandreas@gmail.com>
2023-09-28 21:23:53 -0500

    hello!

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const toggleButton = document.getElementById('menu');
-        const navItems = document.getElementById('nav-items');
+const toggleButtons = document.querySelectorAll('.menu');
+        const navItems = document.querySelector('#nav-items');
 
         // Add a click event listener to the button
-        toggleButton.addEventListener('click', function () {
+        for(var i = 0; i < toggleButtons.length; i++) 
+        toggleButtons[i].addEventListener("click", function() {
             // Toggle the "active" and "inactive" classes on the nav-items element
             navItems.classList.toggle('nav-open');
         });
