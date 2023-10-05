7400ea4ade14308c2755ab94cdb4d9b5067b9e95
lane-andreas <laaaandreas@gmail.com>
2023-10-04 20:43:25 -0500

    final?

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const portfolioImages = document.querySelectorAll('.portfolio-img');
     
-  
-
-    portfolioImages.forEach(image => {
-        image.addEventListener('click', function () {
-          const imgModul = document.querySelector('#img-modul');
-          
-            this.classList.toggle('max-width');
-
-          if (imgModul) {
-            imgModul.remove();
-
-        } else {
-            const newElement = document.createElement('div');
-            newElement.id = 'img-modul';
-            document.body.appendChild(newElement);
-        }
-
-        });
-    });
 
     const toggleButtons = document.querySelectorAll('.menu');
     const navItems = document.querySelector('#nav-items');
@@ -33,25 +13,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
